d07e0a7055e4fa7bace9a734872d0ecbaadc31a1
Yosan <tekleyosan15@gmail.com>
2022-07-08 12:25:59 -0400

    final send

---
+++ javascript.js
@@ -0,0 +1,51 @@
+$(window).on("sroll"),function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('grey');
+    }
+    else{
+        $('nav').removeClass('grey');
+    }
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    element.classList.toggle("light-mode");
+  }
+
+ 
+  
